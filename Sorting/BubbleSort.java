cec4138ff2382e0bc6651cb3a53256c0d7b41e97
tanyamaheshwari-05 <tanya47887@gmail.com>
2026-02-07 11:56:49 +0530

    Practice of BubbleSort

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,20 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr={2,1,10,8,5,7,6};
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+
+        }
+        for(int i=0;i<n;i++){
+        System.out.print(arr[i]+" ");
+        }
+
+    }
+}
