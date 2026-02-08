ad011126fff297f16d377b853503667fc7d8907d
tanyamaheshwari-05 <tanya47887@gmail.com>
2026-02-08 21:59:46 +0530

    Practice of QuickSort algorithm

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,35 @@
+public class QuickSort {
+    public static void quickSort(int[] arr,int low,int high){
+        if(low<high){
+        int pivot=partition(arr,0,high);
+        quickSort(arr, 0,pivot-1);
+        quickSort(arr, pivot+1, high);
+        }
+    }
+    public static int partition(int arr[],int low,int high){
+        int pivot=arr[high];
+        int i=low-1;
+        for(int j=low;j<high;j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp=arr[i];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        i++;
+        int temp=arr[i];
+        arr[i]=arr[high];
+        arr[high]=temp;
+        return i;
+    }
+    public static void main(String[] args) {
+        int []arr=  {4,2,8,5,6,2};
+        int n=arr.length;
+        quickSort(arr,0,n-1);
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" " );
+        }
+        System.out.println();
+    }
+}
