907061fa72d3b1beec51ee31b0d26c786771df3a
tanyamaheshwari-05 <tanya47887@gmail.com>
2026-02-25 10:56:26 +0530

    Creat thread using Thread class

---
+++ Thread/CreateThread.java
@@ -0,0 +1,13 @@
+// package Thread;
+
+// public class CreateThread extends Thread{
+//     public static void main(String[] args) {
+//         CreateThread th= new CreateThread();
+//         th.start();
+//     }
+//     public void run(){
+//         for(int i=10;i>=1;i--){
+//             System.out.println("Value of i : " +i);
+//         }
+//     }
+// }
